8799ba5576370fd31261cee72612904878f9ade8
sazzadrupak <sazzad@oployeelabs.com>
2025-05-22 20:54:35 +0300

    Load dynamic configurations

--- prsls-sls/functions/get-restaurants.mjs
+++ prsls-sls/functions/get-restaurants.mjs
@@ -1,10 +1,12 @@
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
+import middy from '@middy/core';
+import ssm from '@middy/ssm';
 
 const dynamodbClient = new DynamoDB();
 const dynamodb = DynamoDBDocumentClient.from(dynamodbClient);
 
-const defaultResults = parseInt(process.env.default_results);
+const { serviceName, stage } = process.env;
 const tableName = process.env.restaurants_table;
 
 const getRestaurants = async (count) => {
@@ -20,12 +22,21 @@ const getRestaurants = async (count) => {
   return resp.Items;
 };
 
-export const handler = async (event, context) => {
-  const restaurants = await getRestaurants(defaultResults);
+export const handler = middy(async (event, context) => {
+  const restaurants = await getRestaurants(context.config.defaultResults);
   const response = {
     statusCode: 200,
     body: JSON.stringify(restaurants),
   };
 
   return response;
-};
+}).use(
+  ssm({
+    cache: true,
+    cacheExpiry: 1 * 60 * 1000, // 1 minute
+    setToContext: true, // fetches individual parameters and stores them in either the invocation context object or the environment variables. By default, they are stored in the environment variables, but we can use the optional config setToContext to tell the middleware to store them in the context object instead.
+    fetchData: {
+      config: `/${serviceName}/${stage}/get-restaurants/config`,
+    },
+  })
+);
