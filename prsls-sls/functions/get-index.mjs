53c98e278ac958b8d8bc2776ee87382cbf783a18
sazzadrupak <sazzad@oployeelabs.com>
2025-05-22 11:17:38 +0300

    Implement e-2-e test for get-index

--- prsls-sls/functions/get-index.mjs
+++ prsls-sls/functions/get-index.mjs
@@ -51,7 +51,7 @@ export const handler = async (event, context) => {
   const response = {
     statusCode: 200,
     headers: {
-      'Content-Type': 'text/html; charset=UTF-8',
+      'content-type': 'text/html; charset=UTF-8',
     },
     body: html,
   };
--- prsls-sls/tests/steps/when.mjs
+++ prsls-sls/tests/steps/when.mjs
@@ -1,5 +1,61 @@
 const APP_ROOT = '../../';
+import { fromNodeProviderChain } from '@aws-sdk/credential-providers';
+import { AwsClient } from 'aws4fetch';
 import _ from 'lodash';
+
+const mode = process.env.TEST_MODE;
+
+const viaHttp = async (relPath, method, opts) => {
+  const url = `${process.env.rest_api_url}/${relPath}`;
+  console.info(`invoking via HTTP ${method} ${url}`);
+
+  const body = _.get(opts, 'body', null);
+  const headers = {};
+
+  const authHeader = _.get(opts, 'auth');
+  if (authHeader) {
+    headers['Authorization'] = authHeader;
+  }
+
+  let res;
+  if (_.get(opts, 'iam_auth', false) === true) {
+    const credentialProvider = fromNodeProviderChain();
+    const credentials = await credentialProvider();
+    const awsClient = new AwsClient({
+      accessKeyId: credentials.accessKeyId,
+      secretAccessKey: credentials.secretAccessKey,
+      sessionToken: credentials.sessionToken,
+    });
+
+    res = await awsClient.fetch(url, {
+      method,
+      headers,
+      body: body,
+    });
+  } else {
+    res = await fetch(url, {
+      method,
+      headers,
+      body: body,
+    });
+  }
+
+  const respHeaders = {};
+  for (const [key, value] of res.headers.entries()) {
+    respHeaders[key] = value;
+  }
+
+  const respBody =
+    respHeaders['Content-Type'] === 'application/json'
+      ? await res.json()
+      : await res.text();
+
+  return {
+    statusCode: res.status,
+    headers: respHeaders,
+    body: respBody,
+  };
+};
 
 const viaHandler = async (event, functionName) => {
   const { handler } = await import(`${APP_ROOT}/functions/${functionName}.mjs`);
@@ -8,7 +64,7 @@ const viaHandler = async (event, functionName) => {
   const response = await handler(event, context);
   const contentType = _.get(
     response,
-    'headers.Content-Type',
+    'headers.content-type',
     'application/json'
   );
   if (response.body && contentType === 'application/json') {
@@ -17,7 +73,16 @@ const viaHandler = async (event, functionName) => {
   return response;
 };
 
-export const we_invoke_get_index = () => viaHandler({}, 'get-index');
+export const we_invoke_get_index = async () => {
+  switch (mode) {
+    case 'handler':
+      return await viaHandler({}, 'get-index');
+    case 'http':
+      return await viaHttp('', 'GET');
+    default:
+      throw new Error(`unsupported mode: ${mode}`);
+  }
+};
 
 export const we_invoke_get_restaurants = () =>
   viaHandler({}, 'get-restaurants');
